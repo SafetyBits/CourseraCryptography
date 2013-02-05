2d3f0fc75f6e869075eaf197ec11d6dbec543c1a
Agzamov Rustam <agzamovr@gmail.com>
2013-02-05 17:45:35 +0500

    Made little bit of refactoring

--- src/week3/Prog3.java
+++ src/week3/Prog3.java
@@ -9,18 +9,19 @@ import java.security.NoSuchAlgorithmException;
 import week2.Utils;
 
 public class Prog3 {
-	private static final String checkFile = "C:\\Users\\rustam\\Videos\\Coursera\\Cryptography I\\week3\\6 - 1 - Introduction (11 min).mp4";
+	private static final String filePath = "C:\\Users\\rustam\\Videos\\Coursera\\Cryptography I\\week3\\6 - 1 - Introduction (11 min).mp4";
 	// 6 - 2 - Generic birthday attack (16 min).mp4
 	private static MessageDigest digest = null;
 
 	public static void main(String[] args) throws IOException,
 			NoSuchAlgorithmException {
-		File f = new File(checkFile);
+		File f = new File(filePath);
 		RandomAccessFile rf = new RandomAccessFile(f, "r");
 		digest = MessageDigest.getInstance("SHA-256");
 		byte[] chunk = new byte[1024 + digest.getDigestLength()];
-		long index = f.length() - f.length() % 1024;
 		byte[] prevChunkHash = new byte[digest.getDigestLength()];
+
+		long index = f.length() - f.length() % 1024;
 		for (; index >= 0; index -= 1024) {
 			rf.seek(index);
 			int r = rf.read(chunk, 0, 1024);
@@ -34,12 +35,5 @@ public class Prog3 {
 			prevChunkHash = digest.digest();
 		}
 		System.out.println(Utils.toHex(prevChunkHash));
-	}
-
-	public static byte[] createDigest(byte[] data)
-			throws NoSuchAlgorithmException, IOException {
-		digest.update(data);
-		return digest.digest();
-
 	}
 }
