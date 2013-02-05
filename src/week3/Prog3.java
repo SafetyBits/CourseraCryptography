9db7aae5f316bb7123a02f72b64eb784d1414c80
Agzamov Rustam <agzamovr@gmail.com>
2013-02-05 17:37:31 +0500

    Added class for third weeks programming assignment

---
+++ src/week3/Prog3.java
@@ -0,0 +1,45 @@
+package week3;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import week2.Utils;
+
+public class Prog3 {
+	private static final String checkFile = "C:\\Users\\rustam\\Videos\\Coursera\\Cryptography I\\week3\\6 - 1 - Introduction (11 min).mp4";
+	// 6 - 2 - Generic birthday attack (16 min).mp4
+	private static MessageDigest digest = null;
+
+	public static void main(String[] args) throws IOException,
+			NoSuchAlgorithmException {
+		File f = new File(checkFile);
+		RandomAccessFile rf = new RandomAccessFile(f, "r");
+		digest = MessageDigest.getInstance("SHA-256");
+		byte[] chunk = new byte[1024 + digest.getDigestLength()];
+		long index = f.length() - f.length() % 1024;
+		byte[] prevChunkHash = new byte[digest.getDigestLength()];
+		for (; index >= 0; index -= 1024) {
+			rf.seek(index);
+			int r = rf.read(chunk, 0, 1024);
+			if (r < 1024) {
+				digest.update(chunk, 0, r);
+			} else {
+				System.arraycopy(prevChunkHash, 0, chunk, 1024,
+						prevChunkHash.length);
+				digest.update(chunk, 0, chunk.length);
+			}
+			prevChunkHash = digest.digest();
+		}
+		System.out.println(Utils.toHex(prevChunkHash));
+	}
+
+	public static byte[] createDigest(byte[] data)
+			throws NoSuchAlgorithmException, IOException {
+		digest.update(data);
+		return digest.digest();
+
+	}
+}
